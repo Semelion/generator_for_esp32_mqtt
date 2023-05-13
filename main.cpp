ead23c8aeb43a8a55509c2b9688f2fe01953aa9e
Semelion <senya2708@gmail.com>
2023-05-13 10:34:03 +0300

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <fstream>
 #include <string>
 #include <iostream>
-#include <vector>
+#include <set>
 
 #include <nlohmann/json.hpp>
 
@@ -44,42 +44,121 @@ int main () {
     esp_code << "#define MQTT_LOGIN\t" << config["mqtt_broker"]["MQTT_LOGIN"] << std::endl;
     esp_code << "#define MQTT_PW\t" << config["mqtt_broker"]["MQTT_PW"] << "\n" << std::endl;
 
-
-    //std::cout << config["button"].size() << "\n";
+    std::set<std::string> topics;
 	for(int i = 0; i < config["button"].size(); i++){
-		esp_code << "#define " << config["button"].at(i)["name"] << "\t" << pins[i] << "\n";
+		std::string s = config["button"].at(i)["name"];
+		std::string trimmed = s.substr(1, s.length()-1);
+		esp_code << "#define " << s << "\t" << pins[i] << "\n";
 		count_used_pins++;
+		std::string temp = config["button"].at(i)["topic"];
+		topics.insert(temp);
 	}
 
 	for(int i = 0; i < config["relay"].size(); i++){
-		esp_code << "#define " << config["relay"].at(i)["name"] << "\t" << pins[i + count_used_pins] << "\n";
+		std::string s = config["relay"].at(i)["name"];
+		std::string trimmed = s.substr(1, s.length()-1);
+		esp_code << "#define " << s << "\t" << pins[i + count_used_pins] << "\n";
+		std::string temp = config["relay"].at(i)["topic"];
+		topics.insert(temp);
 	}
 
-	esp_code << "WiFiClient espClient;\n";
+	esp_code << "\nWiFiClient espClient;\n";
 	esp_code << "PubSubClient client(espClient);\n\n";
+
+	std::set <std::string> :: iterator it = topics.begin();
+	for (int i = 1; it != topics.end(); i++, it++) {
+		esp_code << "bool state_topic_" << *it << " = 0;\n";
+    }
 
 	///WRITE SETUP////
 	esp_code << "void setup() {\n";
-	esp_code << "\tSerial.begin(9600);\n\n";
+	esp_code << "\tSerial.begin(" << config["Serial_speed"] << ");\n\n";
 
 	for(int i = 0; i < config["button"].size(); i++){
-		esp_code << "\tpinMode(" << config["button"].at(i)["name"] << ", INPUT);\n";
+		std::string s = config["button"].at(i)["name"];
+		std::string trimmed = s.substr(1, s.length()-1);
+		esp_code << "\tpinMode(" << s << ", INPUT);\n";
 	}
 
 	for(int i = 0; i < config["relay"].size(); i++){
-		esp_code << "\tpinMode(" << config["relay"].at(i)["name"] << ", OUTPUT);\n";
+		std::string s = config["relay"].at(i)["name"];
+		s.substr(1, s.length()-1);
+		esp_code << "\tpinMode(" << s << ", OUTPUT);\n";
+	}
+	esp_code << "\n\tconnect2WIFI();\n";
+	esp_code << "\tconnect2mqtt();\n";
+
+	esp_code << "}\n";
+	///END SETUP//////
+
+	///WRITE LOOP/////
+	esp_code << "\nvoid loop(){\n";
+	esp_code << "\tif (!client.connected())\n\t\tconnect2mqtt();\n\telse\n\t\tclient.loop();\n";
+	
+	for(int i = 0; i < config["button"].size(); i++){
+		std::string s = config["button"].at(i)["name"];
+		s.substr(1, s.length()-1);
+		esp_code << "\tif(digitalRead(" << s << ")){\n";
+		esp_code << "\t\twhile(digitalRead(" << s << "));\n";
+		s = config["button"].at(i)["topic"];
+		s.substr(1, s.length()-1);
+		esp_code << "\t\tchar pub[1] = {!state_topic_" << s << " + '0'};\n";
+		esp_code << "\t\tclient.publish(" << config["button"].at(i)["topic"] <<", pub);\n";
+		esp_code << "\t\tdelay(10);\n";
+		esp_code << "\t}\n";
 	}
 	esp_code << "}\n";
+	// if(digitalRead(but1)){
+    //   while(digitalRead(but1));
+    //   char out[1] = {!state_topic_kitchen + 48};
+    //   client.publish("kitchen1", out);
+    //   delay(10);
+    // }
+	// for (int i = 1; it != topics.end(); i++, it++) {
+	// 	esp_code << "if(digitalRead(" << 
+	// 	// esp_code << "bool state_topic_" << *it << " = 0;\n";
+    // }
+	///END LOOP///////
 
-	// //voids//
-	// if (esp_code.is_open() && functions.is_open()){
-	// 	std::cout << "function is opened\n";
-	// 	std::string line;
-	// 	while ( std::getline (functions,line) ){
-	// 		// std::cout << line << "\n";
-	// 		esp_code << line << '\n';
-	// 	}
-	// }
+	////WRITE FUNCTIONS/////
+	esp_code << "\n";
+	if (esp_code.is_open() && functions.is_open()){
+		//std::cout << "start_file is opened\n";
+		std::string line;
+		while ( std::getline (functions,line) ){
+			// std::cout << line << "\n";
+			esp_code << line << '\n';
+		}
+	}
+
+	it = topics.begin();
+	for (int i = 1; it != topics.end(); i++, it++) {
+		esp_code << "\tclient.subscribe(\"" << *it << "\");\n";
+    }
+
+	esp_code << "}\n\n";
+	//callback
+	esp_code << "void callback(String topic, byte* payload, unsigned int length) {\n";
+	esp_code << "\tSerial.print(\"Message arrived in topic: \");\n";
+	esp_code << "\tSerial.println(topic);\n";
+	esp_code << "\tfor (int i = 0; i < length; i++)\n";
+	esp_code << "\t\tSerial.print((char)payload[i]);\n";
+
+	for(int i = 0; i < config["relay"].size(); i++){
+		esp_code << "\tif(topic == " << config["relay"].at(i)["topic"] << "){\n";
+		std::string s = config["relay"].at(i)["topic"];
+		s.substr(1, s.length()-1);
+		esp_code << "\t\tstate_topic_" << s << " = bool((char)payload[0] - '0');\n";
+
+		s = config["relay"].at(i)["name"];
+		s.substr(1, s.length()-1);
+		esp_code << "\t\tdigitalWrite(" << s << ", " << "bool((char)payload[0] - '0') );\n";
+		esp_code << "\t}\n";
+	}
+
+	esp_code << "}";
+	///END FUNCTIONS///////
+
 
 	//CLOSE FILES//
 	// config.close();
